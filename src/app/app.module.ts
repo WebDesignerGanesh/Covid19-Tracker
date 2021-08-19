9713461c245305c8150a5ddc0b0822f832a09cf8
Ganesh Shinde <shindeganesh2082000@gmail.com>
2021-08-19 13:45:23 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+import {HttpClientModule} from '@angular/common/http';
+import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component'
+import { GoogleChartsModule } from 'angular-google-charts';
+import { ChartType, Row } from "angular-google-charts";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent,
+    DashboardCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    GoogleChartsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
